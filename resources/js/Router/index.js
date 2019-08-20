9d6d310ae1ff80cb0560ea53591c3924e6b8b7d1
Vlad <37487589+Vlad0395@users.noreply.github.com>
2019-08-21 00:09:40 +0300

    added style in personalContact

--- resources/js/Router/index.js
+++ resources/js/Router/index.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, {Component} from 'react'
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Example from "../components/Example";
-import Example2 from "../components/Example2";
+import PersonalContact from "../components/PersonalContact";
+
 class Router extends Component {
     render() {
-        return ( < BrowserRouter >
-            <
-            Switch >
-            <
-            Route path = '/'
-            exact component = { Example }
-            /> <
-            Route path = '/ex'
-            component = { Example2 }
-            /> <
-            /Switch> <
-            /BrowserRouter>
+        return (
+            <BrowserRouter>
+                < Switch>
+                    <Route path='/'
+                           exact component={Example}
+                    />
+                    <Route path='/personalcontact'
+                           component={PersonalContact}
+                    />
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
--- resources/js/components/PersonalContact.jsx
+++ resources/js/components/PersonalContact.jsx
@@ -4,22 +4,22 @@ import React, { Component } from 'react'
 class PersonalContact extends Component {
     render() {
         return (
-            <div className='container'>
+            <div className='container card'>
                 <div className="row">
-                    <div className="col-md-8">First Name</div>
-                    <div className="col-md-4">Photo</div>
+                    <div className="col-md-4 ml-md-left">First Name</div>
+                    <div className="col-md-4 ml-md-left">Photo</div>
                 </div>
                 <div className="row">
-                    <div className="col-md-8">Last Name</div>
+                    <div className="col-md-4">Last Name</div>
                 </div>
                 <div className="row">
-                    <div className="col-md-12">Number</div>
+                    <div className="col-md-8 ">Number</div>
                 </div>
                 <div className="row">
-                    <div className="col-md-12">Company</div>
+                    <div className="col-md-8">Company</div>
                 </div>
                 <div className="row">
-                    <div className="col-md-12">e-mail</div>
+                    <div className="col-md-8">e-mail</div>
                 </div>
             </div>
         )
