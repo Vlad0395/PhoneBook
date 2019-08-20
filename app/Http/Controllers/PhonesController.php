76cf3358829464c438ecd5ca8637ff343aff80b4
Rostyslav <ptashinskij.rostik@gmail.com>
2019-08-21 00:32:10 +0300

    some changes

--- app/Http/Controllers/PhonesController.php
+++ app/Http/Controllers/PhonesController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Phone;
 use Illuminate\Http\Request;
 
 class PhonesController extends Controller
 {
-    //
+    public function index()
+    {
+        $phones = Phone::all();
+        return response()->json($phones);
+    }
 }
