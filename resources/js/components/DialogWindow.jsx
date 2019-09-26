f4cae096f1bb9b87737d8ada18cb5e5e7468379e
Vlad <vlad2009_@ukr.net>
2019-09-26 21:57:01 +0300

    added expansion eslint for eddit my code before my commit, and changed components ImagejCrop and DialogWindows all function transferred to the perent component

--- resources/js/components/DialogWindow.jsx
+++ resources/js/components/DialogWindow.jsx
@@ -1,104 +1,113 @@
-import React, { Component } from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import FormControl from "@material-ui/core/FormControl";
-import ImagejCrop from './ImagejCrop'
-import Grid from "@material-ui/core/Grid";
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import FormControl from '@material-ui/core/FormControl';
+import ImagejCrop from './ImagejCrop';
+import Grid from '@material-ui/core/Grid';
 
 const Styles = theme => ({
-    form: {
-        display: "flex",
-        flexDirection: "column",
-        margin: "auto",
-        width: "fit-content"
-    },
-    formControl: {
-        marginTop: theme.spacing(2),
-        minWidth: 120
-    },
-    formControlLabel: {
-        marginTop: theme.spacing(1)
-    }
+	form: {
+		display: 'flex',
+		flexDirection: 'column',
+		margin: 'auto',
+		width: 'fit-content',
+	},
+	formControl: {
+		marginTop: theme.spacing(2),
+		minWidth: 120,
+	},
+	formControlLabel: {
+		marginTop: theme.spacing(1),
+	},
 });
 
 class MaxWidthDialog extends Component {
-    state = {
-        open: false,
-        fullWidth: true,
-        maxWidth: "sm"
-    };
+	state = {
+		open: false,
+		fullWidth: true,
+		maxWidth: 'sm',
+	};
 
-    handleClickOpen = () => {
-        this.setState({
-            open: !this.state.open
-        });
-    };
+	handleClickOpen = () => {
+		this.setState({
+			open: !this.state.open,
+		});
+	};
 
-    handleClose = () => {
-        this.setState({
-            open: !this.state.open
-        });
-    };
+	handleClose = () => {
+		this.setState({
+			open: !this.state.open,
+		});
+	};
 
-    handleMaxWidthChange = event => {
-        this.setState({
-            [event.target.value]: event.target.value
-        });
-    };
+	handleMaxWidthChange = event => {
+		this.setState({
+			[event.target.value]: event.target.value,
+		});
+	};
 
-    handleFullWidthChange = event => {
-        this.setState({
-            [event.target.value]: event.target.value
-        });
-    };
-    render() {
-        const classes = this.props;
-        const {
-            fullWidth,
-            maxWidth,
-            open,
-        } = this.state;
-        return (
-            <Grid>
-                <Button
-                    variant="outlined"
-                    color="primary"
-                    onClick={this.handleClickOpen}
-                >
-                    Open max-width dialog
-                </Button>
-                <Dialog
-                    fullWidth={fullWidth}
-                    maxWidth={maxWidth}
-                    open={open}
-                    onClose={this.handleClose}
-                    aria-labelledby="max-width-dialog-title"
-                >
-                    <DialogTitle id="max-width-dialog-title">Optional sizes</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText>
-                            You can set my maximum width and whether to adapt or not.
-                    </DialogContentText>
-                        <form className={classes.form} noValidate>
-                            <FormControl className={classes.formControl}>
-                                <ImagejCrop/>
-                            </FormControl>
-                        </form>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={this.handleClose} color="primary">
-                            Close
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-            </Grid>
-        );
-    }
+	handleFullWidthChange = event => {
+		this.setState({
+			[event.target.value]: event.target.value,
+		});
+	};
+	render() {
+		const {
+			classes,
+			src,
+			crop,
+			croppedImageUrl,
+			onSelectFile,
+			onImageLoaded,
+			onCropComplete,
+			onCropChange,
+		} = this.props;
+		const { fullWidth, maxWidth, open } = this.state;
+		return (
+			<Grid>
+				<Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+					Open max-width dialog
+				</Button>
+				<Dialog
+					fullWidth={fullWidth}
+					maxWidth={maxWidth}
+					open={open}
+					onClose={this.handleClose}
+					aria-labelledby="max-width-dialog-title"
+				>
+					<DialogTitle id="max-width-dialog-title">Optional sizes</DialogTitle>
+					<DialogContent>
+						<DialogContentText>You can set my maximum width and whether to adapt or not.</DialogContentText>
+						<form className={classes.form} noValidate>
+							<FormControl className={classes.formControl}>
+								<ImagejCrop
+									src={src}
+									crop={crop}
+									croppedImageUrl={croppedImageUrl}
+									onSelectFile={onSelectFile}
+									onImageLoaded={onImageLoaded}
+									onCropComplete={onCropComplete}
+									onCropChange={onCropChange}
+									// getCroppedImg={getCroppedImg}
+									// makeClientCrop={makeClientCrop}
+								/>
+							</FormControl>
+						</form>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={this.handleClose} color="primary">
+							Close
+						</Button>
+					</DialogActions>
+				</Dialog>
+			</Grid>
+		);
+	}
 }
 
 export default withStyles(Styles)(MaxWidthDialog);
--- resources/js/components/FormContact.jsx
+++ resources/js/components/FormContact.jsx
@@ -20,6 +20,79 @@ import FormatedInput from './FormatedInput';
 import DialogWindow from './DialogWindow';
 
 class FormContact extends Component {
+	state = {
+		src: null,
+		crop: {
+			unit: '%',
+			width: 30,
+			aspect: 16 / 9,
+		},
+		croppedImageUrl: null,
+	};
+
+	onSelectFile = e => {
+		if (e.target.files && e.target.files.length > 0) {
+			const reader = new FileReader();
+			reader.addEventListener('load', () => this.setState({ src: reader.result }));
+			reader.readAsDataURL(e.target.files[0]);
+		}
+	};
+
+	// If you setState the crop in here you should return false.
+	onImageLoaded = image => {
+		this.imageRef = image;
+	};
+
+	onCropComplete = crop => {
+		this.makeClientCrop(crop);
+	};
+
+	onCropChange = crop => {
+		this.setState({ crop });
+	};
+
+	async makeClientCrop(crop) {
+		if (this.imageRef && crop.width && crop.height) {
+			const croppedImageUrl = await this.getCroppedImg(this.imageRef, crop, 'newFile.jpeg');
+			this.setState({ croppedImageUrl });
+		}
+	}
+
+	getCroppedImg(image, crop, fileName) {
+		const canvas = document.createElement('canvas');
+		const scaleX = image.naturalWidth / image.width;
+		const scaleY = image.naturalHeight / image.height;
+		canvas.width = crop.width;
+		canvas.height = crop.height;
+		const ctx = canvas.getContext('2d');
+
+		ctx.drawImage(
+			image,
+			crop.x * scaleX,
+			crop.y * scaleY,
+			crop.width * scaleX,
+			crop.height * scaleY,
+			0,
+			0,
+			crop.width,
+			crop.height
+		);
+
+		return new Promise((resolve, reject) => {
+			canvas.toBlob(blob => {
+				if (!blob) {
+					reject(new Error('Canvas is empty'));
+					console.error('Canvas is empty');
+					return;
+				}
+				blob.name = fileName;
+				window.URL.revokeObjectURL(this.fileUrl);
+				this.fileUrl = window.URL.createObjectURL(blob);
+				resolve(this.fileUrl);
+			}, 'image/jpeg');
+		});
+	}
+
 	render() {
 		const {
 			classes,
@@ -32,6 +105,8 @@ class FormContact extends Component {
 			handleChange,
 			ActionWithData,
 		} = this.props;
+		const { src, crop, croppedImageUrl } = this.state;
+
 		let url = window.location.href;
 		let id = url.substring(url.lastIndexOf('/') + 1);
 		return (
@@ -65,8 +140,19 @@ class FormContact extends Component {
 					<Grid className={classes.margin}>
 						<Grid container spacing={1}>
 							<Grid item>
-								<DialogWindow />
+								<DialogWindow
+									src={src}
+									crop={crop}
+									croppedImageUrl={croppedImageUrl}
+									onSelectFile={this.onSelectFile}
+									onImageLoaded={this.onImageLoaded}
+									onCropComplete={this.onCropComplete}
+									onCropChange={this.onCropChange}
+									getCroppedImg={this.getCroppedImg}
+									makeClientCrop={this.makeClientCrop}
+								/>
 								<TextField type="file" name="photo_contact" onChange={handleChange} />
+								<img src={croppedImageUrl} alt="" />
 							</Grid>
 						</Grid>
 						<Grid container spacing={1} alignItems="flex-end">
